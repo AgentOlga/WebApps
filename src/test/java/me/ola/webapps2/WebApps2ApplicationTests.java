23cc898f1e6f22037afc360051fc742fe28aa86a
AgentOlga <agentolga@gmail.com>
2023-02-15 22:03:58 +0100

    Added missing file

---
+++ src/test/java/me/ola/webapps2/WebApps2ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ola.webapps2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApps2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
