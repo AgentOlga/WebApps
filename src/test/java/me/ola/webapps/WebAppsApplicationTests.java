7a697c6a667f8504b6c58af29701597cbe9a2592
AgentOlga <agentolga@gmail.com>
2023-03-10 19:55:03 +0100

    homework Stream API

---
+++ src/test/java/me/ola/webapps/WebAppsApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ola.webapps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
